37a95e98bcc7775b8e95da3d51597f1e86364400
Borja Gonzalez <borja@bgon.es>
2021-09-19 16:34:29 +0200

    refactor: utils/dom utility methods

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,26 +1,50 @@
-export const $ = (selector: string): HTMLElement =>
-  document.querySelector<HTMLElement>(selector)
-
-export const $$ = (selector: string): NodeListOf<HTMLElement> =>
-  document.querySelectorAll(selector)
+const SVG_XMLNS = 'http://www.w3.org/2000/svg'
+const SVG_TAGS = ['svg', 'path', 'rect', 'text', 'g']
 
 type Props = {
+  tag: string,
   classes?: string[]
-  children?: HTMLElement[]
+  children?: Element[]
   text?: string
-  onclick?: (event: MouseEvent) => void
-  [key: string]: unknown
+  onClick?: { primary?: () => void, secondary?: () => void }
+  [key: string]: unknown // TODO: rest props should be string
 }
 
-export const create = (tag: string, props: Props): HTMLElement => {
-  const { classes, children, text, onclick, ...rest } = props
-  const element = document.createElement(tag)
+const isSVGTag = (tag: string): boolean => SVG_TAGS.includes(tag)
 
-  if (text) element.innerText = text
+const defaultPrevented = (callback: () => void) => (e: Event) => {
+  e.preventDefault()
+  callback()
+}
+
+export function create(props: Props, dom: Document): Element {
+  const { tag, classes, children, text, onClick, ...rest } = props
+  const element: Element = isSVGTag(tag)
+    ? dom.createElementNS(SVG_XMLNS, tag)
+    : dom.createElement(tag)
+  
+  if (text) element.textContent = text
   if (children) children.forEach(child => element.appendChild(child))
   if (classes) classes.forEach(className => element.classList.add(className))
-  if (onclick) element.addEventListener('click', onclick)
-  Object.entries(rest).forEach(([key, value]) => element.setAttribute(key, value as string))
-
+  if (onClick?.primary)
+    element.addEventListener('click', defaultPrevented(onClick.primary))
+  if (onClick?.secondary)
+    element.addEventListener('contextmenu', defaultPrevented(onClick.secondary))
+  Object.entries(rest).forEach(([key, value]) =>
+    element.setAttribute(key, value as string)
+  )
+  
   return element
 }
+
+export const $ = (
+  selector: string,
+  parent: Document | Element
+): Element | null =>
+  parent.querySelector(selector)
+
+export const $$ = (
+  selector: string,
+  parent: Document | Element
+): Element[] => 
+  Array.from(parent.querySelectorAll(selector))
