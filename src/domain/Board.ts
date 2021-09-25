c7b8789e13cf58e79a5b43312205956f3a409710
Borja Gonzalez <borja@bgon.es>
2021-09-26 00:11:10 +0200

    fix: error on openAround method

--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -69,7 +69,7 @@ class Board {
   private openAround(x: number, y: number) {
     for (let i = x - 1; i <= x + 1; i++) {
       for (let j = y - 1; j <= y + 1; j++) {
-        if (i === x || j === y) continue
+        if (i === x && j === y) continue
         this.open(i, j)
       }
     }
