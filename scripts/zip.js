fe8c16214f6fdc7e5073257f36ff83735926bb4f
Borja Gonzalez <borja@bgon.es>
2021-09-26 20:43:39 +0200

    build: add right files to zip

--- scripts/zip.js
+++ scripts/zip.js
@@ -9,7 +9,23 @@ const { version } = manifest
 const zip = new JSZip()
 const zipPath = path.resolve(__dirname, `../build/github-mines-${version}.zip`)
 
+const FILES = [
+  'manifest.json',
+  'index.js',
+  'index.css',
+  'icon16.png',
+  'icon32.png',
+  'icon48.png',
+  'icon64.png',
+  'icon128.png'
+]
+
 const options = { type: 'nodebuffer', streamFiles: true }
 
-zip.folder('../build')
+FILES.forEach(file => {
+  const filePath = path.resolve(__dirname, `../build/${file}`)
+  const fileBuffer = fs.readFileSync(filePath)
+  zip.file(file, fileBuffer)
+})
+
 zip.generateNodeStream(options).pipe(fs.createWriteStream(zipPath))
