f99fdde45f630f59d34be92054d0c7cee184baab
Borja Gonzalez <borja@bgon.es>
2021-09-27 01:05:17 +0200

    build: new zip script

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,31 +1,27 @@
 /* eslint-disable */
-const fs = require('fs')
 const path = require('path')
-const JSZip = require('jszip')
+const AdmZip = require('adm-zip')
 
-const manifest = require('../manifest.json')
-const { version } = manifest
+const packageJson = require('../package.json')
+const { name, version } = packageJson
 
-const zip = new JSZip()
-const zipPath = path.resolve(__dirname, `../build/github-mines-${version}.zip`)
+const zip = new AdmZip()
+const targetPath = path.resolve(__dirname, `../build/${name}-${version}.zip`)
 
 const FILES = [
   'manifest.json',
-  'index.js',
-  'index.css',
-  'icon16.png',
-  'icon32.png',
-  'icon48.png',
-  'icon64.png',
-  'icon128.png'
+  'build/index.js',
+  'build/index.css',
+  'icons/icon16.png',
+  'icons/icon32.png',
+  'icons/icon48.png',
+  'icons/icon64.png',
+  'icons/icon128.png'
 ]
 
-const options = { type: 'nodebuffer', streamFiles: true }
-
 FILES.forEach(file => {
-  const filePath = path.resolve(__dirname, `../build/${file}`)
-  const fileBuffer = fs.readFileSync(filePath)
-  zip.file(file, fileBuffer)
+  const filePath = path.resolve(__dirname, `../${file}`)
+  zip.addLocalFile(filePath)
 })
 
-zip.generateNodeStream(options).pipe(fs.createWriteStream(zipPath))
+zip.writeZip(targetPath)
