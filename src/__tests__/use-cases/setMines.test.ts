72b06331cc3a44ba707164c69df2dd04a0d8f298
Borja Gonzalez <borja@bgon.es>
2021-09-26 18:48:06 +0200

    test: use-cases/setMines

---
+++ src/__tests__/use-cases/setMines.test.ts
@@ -0,0 +1,40 @@
+describe('use-cases/setMines', () => {
+  const VALID_STATUS = ['waiting', 'win', 'lose']
+
+  const minesMock = jest.fn((value) => mines = value)
+
+  let mines: number
+  let status: string
+  let setMines: (mines: number) => void
+
+  class GameMock {
+    static getInstance = () => new GameMock()
+    get status() { return status }
+    set status(value) { status = value }
+    get mines() { return mines }
+    set mines(value) { minesMock(value) }
+  }
+
+  beforeEach(() => {
+    jest.isolateModules(() => {
+      jest.mock('domain/Game', () => GameMock)
+      setMines = require('use-cases').setMines
+    })
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
+  it('should do nothing if status is playing', () => {
+    status = 'playing'
+    setMines(10)
+    expect(minesMock).not.toBeCalled()
+  })
+
+  it.each(VALID_STATUS)(
+    'should set mines if status is %p',
+    (validStatus) => {
+      status = validStatus
+      setMines(10)
+      expect(minesMock).toBeCalledWith(10)
+    })
+})
